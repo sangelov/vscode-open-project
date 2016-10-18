504642733d7486180c7a69aa48f7843f031060f7
Svetlozar Angelov <svetlozar.angelov@telerik.com>
2016-10-18 14:40:44 +0300

    Add Change Current Folder command

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 	status.checkCodePath();
 
 	var openProjectCommand = vscode.commands.registerCommand(op.OpenProjectCommandId, op.openProject);
+	var changeProjectCommand = vscode.commands.registerCommand(op.ChangeProjectCommandId, op.changeProject);
 	var showCodeCommandNotFound = vscode.commands.registerCommand(status.CodeNotFoundCommandId, status.showCodeCommandNotFoundMessage);
 	context.subscriptions.push(openProjectCommand);
 }
--- src/openproject.ts
+++ src/openproject.ts
@@ -6,23 +6,39 @@ import * as status from "./status"
 import cp = require("child_process");
 
 export const OpenProjectCommandId = "vscode-open-project.openProject";
+export const ChangeProjectCommandId = "vscode-open-project.changeProject";
 
 export function openProject() {
-	var projects = vscode.workspace.getConfiguration("vscode-open-project").get("projects");
-	var projectNames = Object.getOwnPropertyNames(projects);
-	vscode.window.showQuickPick(projectNames).then(launchNewInstance);
+	selectProject(launchNewInstance);
 }
 
-function launchNewInstance(selected: string) {
-	if (selected) {
-		var projects = vscode.workspace.getConfiguration("vscode-open-project").get("projects");
-		var projectFolder = projects[selected];
-		var codeCommand = path.getCodeCommandPath();
-		if (codeCommand) {
-			cp.execFile(codeCommand, [projectFolder]);
-		} else {
-			status.showCodeCommandNotFoundMessage();
-		}
+export function changeProject() {
+	selectProject(changeCurrentProject);
+}
 
+function selectProject(selectedProjectAction: (folder: string) => void) {
+	var projects = vscode.workspace.getConfiguration("vscode-open-project").get("projects");
+	var projectNames = Object.getOwnPropertyNames(projects);
+	vscode.window.showQuickPick(projectNames)
+		.then(selected => {
+			var projects = vscode.workspace.getConfiguration("vscode-open-project").get("projects");
+			var projectFolder = projects[selected];
+			if (projectFolder) {
+				selectedProjectAction(projectFolder);
+			}
+		});
+}
+
+function changeCurrentProject(selected: string) {
+	let uri = vscode.Uri.parse('file:///' + selected);
+	vscode.commands.executeCommand('vscode.openFolder', uri);
+}
+
+function launchNewInstance(projectFolder: string) {
+	var codeCommand = path.getCodeCommandPath();
+	if (codeCommand) {
+		cp.execFile(codeCommand, [projectFolder]);
+	} else {
+		status.showCodeCommandNotFoundMessage();
 	}
 }
